d574b4b121bc593cf530e4b8a7fa8982a6f4810b
Dheeraj Kumar Gopali <DheerajKumar.Gopali@in.pega.com>
2021-02-18 18:58:05 +0530

    Latest backup

--- gradlew.bat
+++ gradlew.bat
@@ -14,6 +14,13 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+
+##The below code is added by dheeraj to work debugging the framework
+echo before %GRADLE_OPTS%
+set GRADLE_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005
+echo after %GRADLE_OPTS%
+
+
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
